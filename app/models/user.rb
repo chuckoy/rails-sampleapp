f85310aa511018295ec9a02d6d7151170ad8622f
Chuck Logan Lim <chucky_da_great@yahoo.com>
2016-04-10 20:33:53 +1000

    test for uniqueness of email addresses at the application level made and passed. todo: database level uniqueness enforcement

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255}, # length is 255 b/c of db limitations
-									  format: {with: VALID_EMAIL_REGEX}
+									  format: {with: VALID_EMAIL_REGEX},
+									  uniqueness: {case_sensitive: false}
 end
