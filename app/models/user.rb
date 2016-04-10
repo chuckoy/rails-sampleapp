de89d59c87f6b62abfd3fc70b4bb0c9f4b7c8332
Chuck Logan Lim <chucky_da_great@yahoo.com>
2016-04-10 20:28:58 +1000

    test for invalid email addresses fixed and passed'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	validates :name, presence: true, length: {maximum: 50}
-	validates :email, presence: true, length: {maximum: 255} # length is 255 b/c of db limitations
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum: 255}, # length is 255 b/c of db limitations
+									  format: {with: VALID_EMAIL_REGEX}
 end
