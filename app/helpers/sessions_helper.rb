9ec02cd4bb98ad30c9368d506cddc9e252faca38
Chuck Logan Lim <chucky_da_great@yahoo.com>
2016-04-17 17:20:58 +1000

    Tests updated to deal with two cases: * User clicks log out after having already logged out in a different tab * User logs out in browser A and continues their session in browser B Both cases now covered and passed.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,7 @@ module SessionsHelper
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
+	end
 
 	# Returns the current user logged-in (if any)
 	def current_user
@@ -29,8 +30,16 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	# Forgets a persistent session
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
 	# Logs out the current user
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
 	end
