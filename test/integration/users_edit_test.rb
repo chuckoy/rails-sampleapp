bbccba98f34702f1e9dd70a09b36e0bc86973eae
Chuck Logan Lim <set>
2016-04-20 12:45:46 +1000

    Add authentication requirements to edit, update
    Non-authenticated users will now be redirected to the login page when
    trying to access these pages.
    
    Tests for this authentication and redirection created and passed.
    
    The edit integration test has been updated to observe the logged in
    requirement.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+  	log_in_as(@user)
   	get edit_user_path(@user)
   	assert_template 'users/edit'
   	patch user_path(@user), user: { name: "", email: "foo@invalid",
@@ -15,6 +16,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 	end
 
 	test "successful edit" do
+		log_in_as(@user)
 		get edit_user_path(@user)
 		assert_template 'users/edit'
 		name = "John Doe"
