d3b09152887fb5a0ca6f3e98af5b6c0cdfd5f797
Chuck Logan Lim <set>
2016-04-20 12:31:28 +1000

    Add edit user functionality
    Feature is also tested and accepted.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -13,4 +13,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 								  									password_confirmation: "bar"}
 		assert_template 'users/edit'
 	end
+
+	test "successful edit" do
+		get edit_user_path(@user)
+		assert_template 'users/edit'
+		name = "John Doe"
+		email = "joe@example.com"
+		patch user_path(@user), user: { name: name, email: email,
+																		password: "",
+																		password_confirmation: "" }
+		assert_not flash.empty?
+		assert_redirected_to @user
+		@user.reload
+		assert_equal name, @user.name
+		assert_equal email, @user.email
+	end
 end
